39b164fe888826307da79eef62c3ae25f1c50ed9
Karolína Klepáčková <klepackova.karolina@email.cz>
2020-02-26 13:30:33 +0100

    Wrappers added

---
+++ app/src/main/java/cz/blackchameleon/notes/framework/Result.kt
@@ -0,0 +1,6 @@
+package cz.blackchameleon.notes.framework
+
+sealed class Result<T> {
+    data class Success<T>(val data: T) : Result<T>()
+    data class Error<T>(val error: ErrorEntity) : Result<T>()
+}
