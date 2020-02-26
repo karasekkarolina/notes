427ee07772ba92cf8e30a7a40722889adfaa3943
Karolína Klepáčková <klepackova.karolina@email.cz>
2020-02-26 13:39:46 +0100

    Updated result

--- app/src/main/java/cz/blackchameleon/notes/data/OpenNoteSource.kt
+++ app/src/main/java/cz/blackchameleon/notes/data/OpenNoteSource.kt
@@ -1,8 +1,9 @@
 package cz.blackchameleon.notes.data
 
 import cz.blackchameleon.notes.framework.Note
+import cz.blackchameleon.notes.framework.Result
 
 interface OpenNoteSource {
-    suspend fun getOpenNote(): Note
-    suspend fun setOpenNote(note: Note)
+    suspend fun getOpenNote(): Result<Note?>
+    suspend fun setOpenNote(note: Note?)
 }
--- app/src/main/java/cz/blackchameleon/notes/framework/OpenNoteSourceImplementation.kt
+++ app/src/main/java/cz/blackchameleon/notes/framework/OpenNoteSourceImplementation.kt
@@ -1,14 +1,15 @@
 package cz.blackchameleon.notes.framework
 
 import cz.blackchameleon.notes.data.OpenNoteSource
+import cz.blackchameleon.notes.framework.Result
 
 class OpenNoteSourceImplementation : OpenNoteSource {
     private var openNote: Note? = null
 
-    override suspend fun getOpenNote(): Note =
+    override suspend fun getOpenNote(): Result<Note?> =
         openNote ?: throw IllegalStateException("No open note found")
 
-    override suspend fun setOpenNote(note: Note) {
+    override suspend fun setOpenNote(note: Note?) {
         openNote = note
     }
 }
