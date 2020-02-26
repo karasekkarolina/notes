0fd2eb80a829155518c814f5ac12fe1547decc6c
Karolína Klepáčková <klepackova.karolina@email.cz>
2020-02-26 13:03:57 +0100

    Main activity layout added.

--- app/src/main/java/cz/blackchameleon/notes/MainActivity.kt
+++ app/src/main/java/cz/blackchameleon/notes/MainActivity.kt
@@ -1,5 +1,7 @@
 package cz.blackchameleon.notes
 
+import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import cz.blackchameleon.notes.presentation.base.OnBackPressedListener
@@ -7,6 +9,16 @@ import cz.blackchameleon.notes.presentation.base.OnBackPressedListener
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navHost: NavHostFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
+        navHost = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+    }
 
     override fun onBackPressed() {
         navHost.let { hostFragment ->
